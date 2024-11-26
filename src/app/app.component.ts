b11cec22bf0265d1a1aa5981b29c0a8ab63dbe4b
Breno Abreu <brenogpa@gmail.com>
2024-11-26 16:48:41 -0300

    creating home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { CarouselComponent } from './carousel/carousel.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from "./views/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CarouselComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
